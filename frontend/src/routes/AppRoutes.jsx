6f67e46b6b54404b4f144c542df6c07bba6673d9
Lim Kien Aik <kienaik0903@gmail.com>
2025-04-05 18:27:46 +0800

    Initialize ShadCn

---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const AppRoutes = () => {
+  return (
+    <Button className = "p-10cd">App Routes</Button>
+  )
+}
+
+export default AppRoutes
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+import path from "path";
+import dotenv from "dotenv";
+dotenv.config();
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/server": {
+        target: process.env.VITE_API_BASE_URL,
+      },
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss(), autoprefixer()],
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
