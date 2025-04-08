3e49782bae14a2f558c220e4fff9396f3b37b5f3
Lim Kien Aik <kienaik0903@gmail.com>
2025-04-09 01:48:37 +0800

    Added accept order route

--- backend/routes/order.routes.js
+++ backend/routes/order.routes.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { createOrder } from "../controllers/order.controller.js";
+import { createOrder, acceptOrder } from "../controllers/order.controller.js";
 
 const router = express.Router();
 router.post("/createOrder", createOrder);
+router.patch("/:id/accept", acceptOrder);
 
 export default router;
