2159506f297b26086d4371f9cb20bf637b797faa
Lim Kien Aik <kienaik0903@gmail.com>
2025-08-27 01:05:19 +0800

    small typo

--- backend/routes/restaurant.routes.js
+++ backend/routes/restaurant.routes.js
@@ -1,9 +1,7 @@
 import express from "express";
-import { createRestaurant, getRestaurants } from "../controllers/restaurant.controller.js";
+import { createRestaurant } from "../controllers/restaurant.controller.js";
 
 const router = express.Router();
 router.post("/createRestaurant", createRestaurant);
-router.get("/", getRestaurants);
-router.get()
 
 export default router;
